a9be6d0c0664468960a7b3b980b4ef781d53137c
JHiggins01 <hackjiggins1@gmail.com>
2025-04-14 13:56:40 -0400

    trimmed data and saved to csv

--- read.py
+++ read.py
@@ -8,4 +8,22 @@ Created on Mon Apr 14 12:51:21 2025
 
 import pandas as pd
 
+#reading in data
 trade = pd.read_csv("trade-register.csv")
+
+unique_values = trade['Recipient'].unique().tolist()
+
+#removing non-country records
+trimmed_trade = trade[~trade['Recipient'].str.endswith('*')]
+trimmed_trade = trimmed_trade[~trade['Recipient'].str.endswith(')')]
+
+#checking
+unique_values = trimmed_trade['Recipient'].unique().tolist()
+
+#dropping unneeded columns
+trimmed_trade = trimmed_trade.drop(columns = ['Order date is estimate',
+                                              'Numbers delivered is estimate',
+                                              'Delivery year is estimate'])
+
+#saving to a csv
+trimmed_trade.to_csv('trimmed_trade.csv')
