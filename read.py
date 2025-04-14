ac649ce6999e368f606027d8ff21f878143e51d5
JHiggins01 <hackjiggins1@gmail.com>
2025-04-14 13:13:55 -0400

    getting data

---
+++ read.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 14 12:51:21 2025
+
+@author: jackhiggins
+"""
+
+import pandas as pd
+
+trade = pd.read_csv("trade-register.csv")
