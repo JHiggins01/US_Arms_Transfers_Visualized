82cc25e00237efb62670252d2e6a90585ee59605
JHiggins01 <hackjiggins1@gmail.com>
2025-04-17 09:21:22 -0400

    trimming columns

--- read.py
+++ read.py
@@ -23,7 +23,11 @@ unique_values = trimmed_trade['Recipient'].unique().tolist()
 #dropping unneeded columns
 trimmed_trade = trimmed_trade.drop(columns = ['Order date is estimate',
                                               'Numbers delivered is estimate',
-                                              'Delivery year is estimate'])
+                                              'Delivery year is estimate',
+                                              'Supplier', 'Local production',
+                                              'Status'])
+#renaming columns for ease
+trimmed_trade = trimmed_trade.rename(columns = {'TIV delivery values':'Total TIV'})
 
 #saving to a csv
 trimmed_trade.to_csv('trimmed_trade.csv')
@@ -32,9 +36,13 @@ trimmed_trade.to_csv('trimmed_trade.csv')
 #grouping by different values
 ################################
 #group by country by year
-by_year = trimmed_trade.groupby(['Recipient', 'Delivery year'])['TIV delivery values'].sum().reset_index()
-
+by_year = trimmed_trade.groupby(['Recipient', 'Delivery year'])['Total TIV'].sum().reset_index()
 #group by country only
-by_country = by_year.groupby(['Recipient'])['TIV delivery values'].sum().reset_index()
+by_country = by_year.groupby(['Recipient'])['Total TIV'].sum().reset_index()
 
-#grouping by weapon type
+#grouping by weapon category
+#checking types
+types = trimmed_trade['Armament category'].unique().tolist()
+by_cat_tot = trimmed_trade.groupby('Armament category')['Total TIV'].sum().reset_index()
+by_cat_year = trimmed_trade.groupby(['Armament category','Delivery year'])['Total TIV'].sum().reset_index()
+by_cat_country = trimmed_trade.groupby()
